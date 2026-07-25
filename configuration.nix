78abb0bd18fda1263beea6cf8494eeb8e41d57fc
retsudev <gamershpw@gmail.com>
2026-07-25 12:35:13 +0300

    add lazygit and waybar module in h-m and jetbrains mono font

--- configuration.nix
+++ configuration.nix
@@ -6,11 +6,6 @@
     "flakes"
   ];
 
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
--- home.nix
+++ home.nix
@@ -6,6 +6,46 @@
 
 	programs.home-manager.enable = true;
 
+	home.packages = with pkgs; [
+	  nerd-fonts.jetbrains-mono
+	];
+	fonts,fontconfig.enable = true;
+
+	programs.waybar = {
+	  enable = true;
+	  settings = {
+	    mainBar = {
+	      layer = "top";
+	      position = "top";
+	      height = 30;
+	      modules-left = [ "hyprland/workspaces" ];
+	      modules-center = [ "clock" ];
+	      modules-right = [ "wireplumber" "network" "battery" ];
+
+	      wireplumber = {
+	        format = "{volume}% {icon}";
+		format-muted = "";
+		format-icons = [ "" "" "" ];
+	      };
+	    };
+	  };
+	  style = ''
+	  * { font-family; "JetBrainsMono Nerd Font"; }
+	  '';
+	};
+
+	programs.lazygit = {
+	  enable = true;
+	  settings = {
+	    gui.theme.selectedLineBgColor = [ "reverse" ];
+	    os.editPreset = "nvim";
+	    git.paging = {
+	      colorArg = "always";
+	      pager = "delta --dark --paging=never";
+	    };
+	  };
+	};
+
 	programs.git = {
 	  enable = true;
 	  settings = { 

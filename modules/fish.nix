8533061e466b87bd215a039e82341115cd2cbb2b
retsudev <gamershpw@gmail.com>
2026-07-30 02:10:49 +0300

    add more fish aliases

--- modules/fish.nix
+++ modules/fish.nix
@@ -10,6 +10,19 @@
       zen = "zen & disown";
       ls = "ls --color=auto";
       vpn = "Throne & disown";
+      cls = "clear";
+      ff = "clear && fastfetch";
+      #NixOS flake.nix aliases
+      nixf-rs    = "sudo nixos-rebuild switch --flake ~/nixos-config#nixos";
+      nixf-rb    = "sudo nixos-rebuild build --flake ~/nixos-config#nixos";
+      nixf-list  = "nixos-rebuild list-generations";
+      nixf-updts = "nix flake update"; 
+      nixf-dgens = "sudo nix-env --delete-generations old --profile /nix/var/nix/profiles/system";
+      #NixOS home-manager standalone aliases
+      home-sf   = "home-manager switch --flake ~/nixos-config#retsudev";
+      home-bf   = "home-manager build --flake ~/nixos-config#retsudev";
+      home-list = "home-manager generations";
+      home-rm   = "home-manager remove-generations";
     };
   };
 }

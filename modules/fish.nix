3cd0d7f108092bdcb096cac83dc24d5b5e7dfebe
retsudev <gamershpw@gmail.com>
2026-07-30 22:37:49 +0300

    change aliases

--- modules/fish.nix
+++ modules/fish.nix
@@ -7,22 +7,20 @@
       set fish_greeting
     '';
     shellAliases = {
-      zen = "zen & disown";
+      # Basic aliases 
       ls = "ls --color=auto";
-      vpn = "Throne & disown";
-      cls = "clear";
       ff = "clear && fastfetch";
-      #NixOS flake.nix aliases
-      nixf-rs    = "sudo nixos-rebuild switch --flake ~/nixos-config#nixos";
-      nixf-rb    = "sudo nixos-rebuild build --flake ~/nixos-config#nixos";
-      nixf-list  = "nixos-rebuild list-generations";
-      nixf-updts = "nix flake update"; 
-      nixf-dgens = "sudo nix-env --delete-generations old --profile /nix/var/nix/profiles/system";
-      #NixOS home-manager standalone aliases
-      home-sf   = "home-manager switch --flake ~/nixos-config#retsudev";
-      home-bf   = "home-manager build --flake ~/nixos-config#retsudev";
-      home-list = "home-manager generations";
-      home-rm   = "home-manager remove-generations";
+      # NixOS flake.nix aliases
+      nix-fswitch  = "sudo nixos-rebuild switch --flake ~/nixos-config#nixos";
+      nix-fbuild   = "sudo nixos-rebuild build --flake ~/nixos-config#nixos";
+      nix-flist    = "nixos-rebuild list-generations";
+      nix-fupdate  = "nix flake update"; 
+      nix-fdelete  = "sudo nix-env --delete-generations old --profile /nix/var/nix/profiles/system";
+      # NixOS home-manager standalone aliases
+      home-fswitch  = "home-manager switch --flake ~/nixos-config#retsudev";
+      home-fbuild   = "home-manager build --flake ~/nixos-config#retsudev";
+      home-listgens = "home-manager generations";
+      home-rmgens   = "home-manager remove-generations";
     };
   };
 }
